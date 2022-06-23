ad0fd0d174903be1dcd75b4e31162a097605c858
unknown <rafahmp123@gmail.com>
2022-06-23 17:48:08 -0300

    Adiciona desafio

---
+++ src/Components/Contato.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import styles from './Contato.module.css';
+import foto from '../img/contato.jpg';
+import Head from './Head';
+
+const Contato = () => {
+  return (
+    <section className={`${styles.contato} + animeLeft`}>
+      <Head title="Ranek | Contato" description="Entre em contato" />
+      <img src={foto} alt="Maquina de escrever" />
+      <div>
+        <h1>Entre em contato</h1>
+        <ul className={styles.dados}>
+          <li>rafaelhmpacheco@gmail.com</li>
+          <li>81 99999-9999</li>
+          <li>Av. Rosa e Silva, 1901</li>
+        </ul>
+      </div>
+    </section>
+  );
+};
+
+export default Contato;
